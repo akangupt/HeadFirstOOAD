a554e882283370b80214a7bcefa10087270eb687
Akanksha Gupta <guptakan@amazon.com>
2018-04-11 21:10:31 +0530

    Duplicate code detected, placed automatic closing of the door in right place

--- src/chapter2/BarkRecognizer.java
+++ src/chapter2/BarkRecognizer.java
@@ -1,8 +1,5 @@
 package chapter2;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class BarkRecognizer {
 
     private DogDoor door;
@@ -13,15 +10,6 @@ public class BarkRecognizer {
     public void recognize(String bark) {
         System.out.println("BarkRecognizer recognized a " + bark);
         door.open();
-
-        final Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                door.close();
-                timer.cancel();
-            }
-        }, 5000);
     }
 
 
