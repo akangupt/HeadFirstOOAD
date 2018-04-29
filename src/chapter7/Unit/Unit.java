52e9396c925f26e614b5609766112d0a11f3758a
Akanksha Gupta <guptakan@amazon.com>
2018-04-29 09:00:19 +0530

    Unit class is to represent army, troops etc. Different unit types may have different properties. Instead of creating different classes use a map to save properties

--- src/chapter7/Unit/Unit.java
+++ src/chapter7/Unit/Unit.java
@@ -1,4 +1,32 @@
 package chapter7.Unit;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Unit {
+
+    private String type;
+    private Map properties;
+
+    public Unit(String type, Map prop) {
+        this.type = type;
+        if(prop == null) this.properties = new HashMap();
+        else this.properties = prop;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Map getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Map properties) {
+        this.properties = properties;
+    }
 }
